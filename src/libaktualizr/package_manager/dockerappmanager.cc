85d3e482fe9afd40c18507f0bdb7b0290a62a26d
Andy Doan <andy@foundries.io>
2019-10-25 09:26:40 -0500

    [fio toup] packagemanager: Clean up unused images after install
    Docker builds up old images and volumes over time consuming disk space.
    This is normally not what you want for a managed device.
    
    Signed-off-by: Andy Doan <andy@foundries.io>
    Signed-off-by: Michael Scott <mike@foundries.io>

--- src/libaktualizr/package_manager/dockerappmanager.cc
+++ src/libaktualizr/package_manager/dockerappmanager.cc
@@ -161,7 +161,15 @@ data::InstallationResult DockerAppManager::install(const Uptane::Target &target)
     return DockerApp(app, config).start();
   };
   if (!iterate_apps(target, cb)) {
-    return data::InstallationResult(data::ResultCode::Numeric::kInstallFailed, "Could not render docker app");
+    res = data::InstallationResult(data::ResultCode::Numeric::kInstallFailed, "Could not render docker app");
+  }
+  if (config.docker_prune) {
+    LOG_INFO << "Pruning unused docker images";
+    // Utils::shell which isn't interactive, we'll use std::system so that
+    // stdout/stderr is streamed while docker sets things up.
+    if (std::system("docker image prune -a -f") != 0) {
+      LOG_WARNING << "Unable to prune unused docker images";
+    }
   }
   return res;
 }

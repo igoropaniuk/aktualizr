6916b207cba41ddbe293a4f8df7611c3d1d4fe35
Andy Doan <andy@foundries.io>
2020-02-27 13:06:02 -0600

    [fio toup] dockerappmanager: notify bootloader before ostree install
    OStree is known to be running on the primary ECU, so notify the
    bootloader before performing the install process for rollback support.
    
    Signed-off-by: Ricardo Salveti <ricardo@foundries.io>
    Signed-off-by: Andy Doan <andy@foundries.io>

--- src/libaktualizr/package_manager/packagemanagerfake.h
+++ src/libaktualizr/package_manager/packagemanagerfake.h
@@ -20,7 +20,7 @@ class PackageManagerFake : public PackageManagerInterface {
   data::InstallationResult install(const Uptane::Target &target) const override;
   void completeInstall() const override;
   data::InstallationResult finalizeInstall(const Uptane::Target &target) override;
-  void updateNotify() override { bootloader_->updateNotify(); };
+  void updateNotify() const override { bootloader_->updateNotify(); };
   bool fetchTarget(const Uptane::Target &target, Uptane::Fetcher &fetcher, const KeyManager &keys,
                    FetcherProgressCb progress_cb, const api::FlowControlToken *token) override;
 

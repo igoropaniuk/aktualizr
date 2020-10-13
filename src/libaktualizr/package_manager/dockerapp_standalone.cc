ba973578402757fd929708716207a6c27aa7eb5a
Mike Sul <mike.sul@foundries.io>
2020-10-13 13:33:16 +0300

    [fio toup] dockerapp: Adjust to removing constness
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- src/libaktualizr/package_manager/dockerapp_standalone.cc
+++ src/libaktualizr/package_manager/dockerapp_standalone.cc
@@ -170,7 +170,7 @@ data::InstallationResult DockerAppStandalone::install(const Uptane::Target &targ
   if (current.sha256Hash() != target.sha256Hash()) {
     // notify the bootloader before installation happens as it is not atomic
     // and a false notification doesn't hurt with rollback support in place
-    updateNotify();
+    const_cast<DockerAppStandalone*>(this)->updateNotify();
     res = OstreeManager::install(target);
     if (res.result_code.num_code == data::ResultCode::Numeric::kInstallFailed) {
       LOG_ERROR << "Failed to install OSTree target, skipping Docker Apps";
